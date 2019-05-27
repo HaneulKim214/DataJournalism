cc1fdedacb89088160f483984bf6105f29ef15b1
haneul kim <haneulkim214@gmail.com>
2019-05-27 18:31:06 -0400

    moving stuff to laptop

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,18 +1,17 @@
 function responsive(){
-
   // If you don't have this it will create multiple svg
   var svgArea = d3.select("body").select("svg");
   // remove if there is already svg
   if (!svgArea.empty()) {
     svgArea.remove();}
-  
+
   // ------------ Now time to create our new svg that is responsive
   var svgWidth = window.innerWidth;
-  var svgHeight = window.innerHeight/2;
+  var svgHeight = window.innerHeight/1.5;
 
-  var groupWidth = svgWidth/1.2;
-  var groupHeight = svgHeight/1.2;
-  
+  var groupWidth = svgWidth/1.3;
+  var groupHeight = svgHeight/1.3;
+
   // Lets append svg tag with attr as created above into <div id="scatter">
   var svg = d3.select("#scatter")
     .append("svg")
@@ -22,7 +21,30 @@ function responsive(){
   // now chart that will go inside svg box. group them together so they can be 
   // moved all at once
   var scatterChartGroup = svg.append("g")
-    .attr("transform", `translate(50, ${svgHeight/20})`)
+    .attr("transform", `translate(100, ${svgHeight/20})`)
+
+  // -------------- Setting initial data
+  var CurrentXLabel = "poverty"; // need to be name of key so it can be called in changeXscale function 
+  var CurrentYLabel = "healthcare";
+
+  // ------------ Function for changing x-Axis label scale
+  function changeXscale(fulldata, clicked_xLabel){
+    var xArray = fulldata.map(d => d[clicked_xLabel]);
+    // Creating new scale with data that corresponds to clicked Label
+    console.log(fulldata, clicked_xLabel, xArray);
+    var newXscale = d3.scaleLinear()
+      .domain([d3.min(xArray) * 0.8 , d3.max(xArray * 1.2)]) // since we don't want our first and last point to be at very end.
+      .range([0, groupHeight]);
+
+    return newXscale;
+  }
+
+  function changeXAxis(clicked_xScale, xAxis) {
+    // when this function is called transition current to new axis
+    xAxis.transition()
+      .duration(1000)
+      .call(d3.axisBottom(clicked_xScale)) //new one with new scale
+  };
 
   // --------------- Loading Data
   // Since we are using d3 version 5.
@@ -30,26 +52,23 @@ function responsive(){
   function error(error) {throw err;}
   // if no error run this function
   function successCall(fulldata){
-    console.log(fulldata);
 
     // since d3.csv read everything into string, convert numbers to int
     fulldata.forEach(function(data){
-      data.healthcare = +data.healthcare;
+      // y-axes
+      data.obesity = +data.obesity;
       data.smokes = +data.smokes;
-      data.age = +data.age;
+      data.healthcare = +data.healthcare;
+
+      // goes in x-axes
       data.poverty = +data.poverty;
+      data.age = +data.age;
+      data.income = +data.income;
     });
     
-    // ------------------ Part I: Scatter plot Healthcare(y) vs. Poverty(x)
-    
-    // just for me to find our min and max of healthcare data
-    var pov = fulldata.map(d => d.poverty);
-    console.log(d3.extent(pov));
-    // min is 9.2 so lets start x-axis from 8
-    var xScale = d3.scaleLinear()
-    .domain([8, d3.max(pov) + 2])
-    .range([0, groupWidth])
-
+    // ------------------ Setting Inital scale, axis, circles, and Text on circles
+    var xScale = changeXscale(fulldata, CurrentXLabel);
+    console.log(xScale(fulldata[1][CurrentXLabel]));
 
     var hc = fulldata.map(d => d.healthcare);
     console.log(d3.extent(hc));
@@ -73,7 +92,7 @@ function responsive(){
       .data(fulldata)
       .enter()
       .append("circle")
-      .attr("cx", d => xScale(d.poverty))
+      .attr("cx", d => xScale(d[CurrentXLabel]))
       .attr("cy", d => yScale(d.healthcare))
       .attr("r", 10)
       .attr("fill", "green")
@@ -86,30 +105,82 @@ function responsive(){
       .data(fulldata)
       .enter()
       .append("text")
-      .attr("x", d => xScale(d.poverty))
+      .attr("x", d => xScale(d[CurrentXLabel]))
       .attr("y", d => yScale(d.healthcare) + 3) // +3 to move it down a bit.
       .style("fill", "white")
       .style("font-size", "10px")
       .style("text-anchor", "middle")
       .text(d => d.abbr) // fulldata have abbr for each city in U.S.
 
+    // ----------- group for all three x-axes labels
+    var xAxesGroup = scatterChartGroup.append("g")
+      .attr("transform", `translate(${groupWidth/2}, ${groupHeight + 35 })`);
+    
+    // add x-axis label to xAxesGroup, assign it to name so it can be called
+    var xLabelPoverty = xAxesGroup.append("text")
+      .classed("aText", true) // give it a class to get style from css
+      .attr("value", "poverty")
+      .text("Poverty rate (%)")
+    
+    var xLabelAge = xAxesGroup.append("text")
+      .classed("aText", true) // give it a class to get style from css
+      .attr("y", 25)
+      .attr("value", "age")
+      .text("Age (Median)")
 
-      // axes Labels: y-axis
-      scatterChartGroup.append("text")
+    var xLabelIncome = xAxesGroup.append("text")
+      .classed("aText", true) // give it a class to get style from css
+      .attr("y", 50)
+      .attr("value", "income")
+      .text("Household Income (Median)")
+    
+    // ------------ group all y-axes labels
+    var yAxesGroup = scatterChartGroup.append("g")
+      .attr("transform", `translate(0,${groupHeight/2})`);
+
+    var yLabelHealth = yAxesGroup.append("text")
       .classed("aText", true)
       .attr("transform", "rotate(-90)")
-      .attr("x", -(groupHeight/2))
-      .attr("y", 0 - 50)
-      .attr("dy", "1em")
-      .attr("text-anchor", "middle")
-      .text("% of people lacking HealthCare");
+      .attr("y", -25)
+      .attr("value", "healthcare") // so it can be reached on event listener
+      .text("population lacking HealthCare (%)")
+    var yLabelSmoke = yAxesGroup.append("text")
+      .classed("aText", true)
+      .attr("transform", "rotate(-90)")
+      .attr("y", -50)
+      .attr("value", "smokes")
+      .text("Smoking population (%)")
+    var yLabelObese = yAxesGroup.append("text")
+      .classed("aText", true)
+      .attr("transform", "rotate(-90)")
+      .attr("y", -75)
+      .attr("value", "obesity")
+      .text("Obese (%)")
 
-      // axes Labels: x-axis
-      scatterChartGroup.append("text")
-        .classed("aText", true) // give it a class to get style from css
-        .attr("transform", `translate(${groupWidth/2}, ${groupHeight + 35 })`)
-        .text("Poverty rate (%)")
+    // // axes Labels: y-axis
+    // scatterChartGroup.append("text")
+    //   .classed("aText", true)
+    //   .attr("transform", "rotate(-90)")
+    //   .attr("x", -(groupHeight/2))
+    //   .attr("y", 0 - 50)
+    //   .attr("dy", "1em")
+    //   .attr("text-anchor", "middle")
+    //   .text("% of people lacking HealthCare");
 
+    // Event listener for xAxes labels
+    xAxesGroup.selectAll("text").on("click", function(){ // run this function when any xAxes label clicked
+      var clicked_xLabel = d3.select(this).attr("value");
+      if (clicked_xLabel != CurrentXLabel){
+        console.log(`You've clicked ${clicked_xLabel}`)
+
+        // now clicked axis become current one.
+        CurrentXAxis = clicked_xLabel;
+        // updating xScale, xAxis, Circles, and text
+        var clicked_xScale = changeXscale(fulldata, clicked_xLabel);
+
+        var clicked_XAxis = changeXAxis(clicked_xScale, xAxis);
+      }
+    });
   }; // end of successCall
 
 };
